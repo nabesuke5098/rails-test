8a86373f75356d4e0bf867270e6244954ff394ab
nabesuke <nabesuke5098@gmail.com>
2021-08-18 08:15:46 +0900

    [add]test case

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,11 +18,31 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe '#age' do
+    before do
+      allow(Time.zone).to receive(:now).and_return(Time.zone.parse('2021/08/18'))
+    end
+
     context '20年前の生年月日の場合' do
       let(:user) { User.new(birthday: Time.zone.now - 20.years) }
 
       it '年齢が20歳であること' do
         expect(user.age).to eq 20
+      end
+    end
+
+    context '10年前に生まれた場合で丁度誕生日の場合' do
+      let(:user) { User.new(birthday: Time.zone.parse('2011/08/18')) }
+
+      it '年齢が10歳であること' do
+        expect(user.age).to eq 10
+      end
+    end
+
+    context '10年前に生まれた場合で誕生日前日である場合' do
+      let(:user) { User.new(birthday: Time.zone.parse('2011/08/19')) }
+
+      it '年齢が9歳であること' do
+        expect(user.age).to eq 9
       end
     end
   end
